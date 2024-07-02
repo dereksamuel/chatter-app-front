58a00415307731fb93d02f70e0fc52584f69304d
Derek Paul <derek.paul@brilliant360.net>
2024-07-02 16:44:04 -0500

    first view to svelte another props object with the same name as the current view

---
+++ src/lib/components/InputGroup.svelte
@@ -0,0 +1,4 @@
+<script>
+  export let
+</script>
+<input type="text" class="input" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import InputGroup from "../lib/components/InputGroup.svelte";
+</script>
+<main class="chatter-group">
+  <p>Welcome</p>
+  <InputGroup placeholder="Search..."></InputGroup>
+</main>
